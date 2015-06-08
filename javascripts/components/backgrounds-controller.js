2f8054edceb827f1c06c24d7d8d45b08976ff448
Dominick Guzzo <dguzzo@gmail.com>
2015-06-07 21:18:02 -0700

    move cycle logic into controller

--- javascripts/components/backgrounds-controller.js
+++ javascripts/components/backgrounds-controller.js
@@ -1,11 +1,17 @@
 angular.module('parallax')
 .controller('BackgroundsController', ["$scope", function($scope) {
+  var cycleLength = 4;
+
   $scope.backgroundCount = 14;
-  $scope.cycleLength = 4;
 
   // http://stackoverflow.com/a/16824944/626369
   $scope.getNumber = function(num) {
     return new Array(num);   
   };
-  
+
+  // determine if we're at a certain part of the cycle. [for determining the DOM components of parallax group]
+  $scope.indexCycleMod = function (index, remainder) {
+    return ((index + 1) % cycleLength === remainder);
+  };
+    
 }]);
